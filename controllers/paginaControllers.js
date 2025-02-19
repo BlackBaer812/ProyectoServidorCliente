3cae204474b823dbe4f2c6cf95d4eb2718798479
Marcos Ruiz Clemente <marcosruiz_8@hotmail.com>
2025-02-19 11:49:05 +0100

    Creada la vista de registro

--- controllers/paginaControllers.js
+++ controllers/paginaControllers.js
@@ -1,8 +1,15 @@
 /* Aqui hay que importar los modelos que vayamos a usar */
 
 const paginaInicio = async (req, res) => {
+    req.session;
+
+    let identificado = false;
+
+    req.session==undefined?identificado=false:identificado=true
+
     res.render("indice", {
-        titulo: "Inicio"
+        titulo: "Inicio",
+        identificado
     });
 }
 
